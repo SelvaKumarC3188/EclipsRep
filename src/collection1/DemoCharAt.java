24c17d4de80ceb8669afe235f3db68f590a80133
SelvaKumarC3188 <selvak070@gmail.com>
2024-03-02 12:22:47 +0530

    update

--- src/collection1/DemoCharAt.java
+++ src/collection1/DemoCharAt.java
@@ -7,12 +7,13 @@ public class DemoCharAt {
 		char c2=ch[1].charAt(0);
 		char c3=ch[2].charAt(0);
 		char c4=ch[3].charAt(0);
-		
+		char c5=ch[4].charAt(0);
 		ArrayList<Character> cha=new ArrayList<>();
 		cha.add(c1);
 		cha.add(c2);
 		cha.add(c3);
 		cha.add(c4);
+		cha.add(c5);
 		for(int i=0;i<cha.size();i++) {
 			System.out.println(cha.get(i));
 		}
